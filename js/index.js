66da22bc240af61db987e7cc6363b96fad6f8ae8
Julia-Frost <juliafrostwork@gmail.com>
2022-05-30 22:02:32 +0300

    add JS

--- js/index.js
+++ js/index.js
@@ -6,3 +6,17 @@ L.marker([55.7537491511233, 37.61986158930323])
   .addTo(map)
   .bindPopup("АТБ-маркет")
   .openPopup();
+
+const createElem = (tag, attr) => {
+  const elem = document.createElement(tag);
+
+  return Object.assign(elem, { ...attr });
+};
+
+const productTitle = document.querySelectorAll(".product__title");
+const productDescription = document.querySelectorAll(".product__description");
+const productButton = document.querySelectorAll(".product__button");
+
+for (let i = 0; i < productButton.length; i++) {
+  console.log(productTitle[i]);
+}
