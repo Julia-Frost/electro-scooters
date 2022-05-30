23a0fc4f80de46f733b70a4f816a640bc46c932d
Julia-Frost <juliafrostwork@gmail.com>
2022-05-30 23:04:24 +0300

    add script

--- js/index.js
+++ js/index.js
@@ -13,10 +13,48 @@ const createElem = (tag, attr) => {
   return Object.assign(elem, { ...attr });
 };
 
+const createModal = (title, description) => {
+  const overlayElem = createElem("div", { className: "modal" });
+  const modalElem = createElem("div", { className: "modal__block" });
+  const modalContainerElem = createElem("div", {
+    className: "modal__container",
+  });
+
+  const titleElem = createElem("h2", {
+    className: "modal__title",
+    textContent: `Заказать ${title}`,
+  });
+
+  const descriptionElem = createElem("p", {
+    className: "modal__desctiprion",
+    textContent: description,
+  });
+
+  const formElem = createElem("form", {
+    className: "modal__form",
+    method: "post",
+    action: "https://jsonplaceholder.typicode.com/posts",
+  });
+
+  modalContainerElem.append(titleElem, descriptionElem);
+
+  modalElem.append(modalContainerElem);
+
+  overlayElem.append(modalElem);
+  return overlayElem;
+};
+
 const productTitle = document.querySelectorAll(".product__title");
 const productDescription = document.querySelectorAll(".product__description");
 const productButton = document.querySelectorAll(".product__button");
 
 for (let i = 0; i < productButton.length; i++) {
-  console.log(productTitle[i]);
+  productButton[i].addEventListener("click", () => {
+    const title = productTitle[i].textContent;
+    const description = productDescription[i].textContent;
+
+    const modal = createModal(title, description);
+
+    document.body.append(modal);
+  });
 }
