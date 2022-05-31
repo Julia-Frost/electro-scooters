4518dc6ac1f574c771b364e2a1bdd1cf17ce4747
Julia-Frost <juliafrostwork@gmail.com>
2022-05-31 21:49:35 +0300

    add scss corrections

--- js/index.js
+++ js/index.js
@@ -66,7 +66,7 @@ const createModal = (title, description) => {
   });
   const nameInputElem = createElem("input", {
     className: "modal__input",
-    placeholder: "Ведите ваше имя",
+    placeholder: "Введите ваше имя",
     name: "name",
     required: true,
   });
@@ -78,7 +78,7 @@ const createModal = (title, description) => {
   });
   const phoneInputElem = createElem("input", {
     className: "modal__input",
-    placeholder: "Ведите ваш телефон",
+    placeholder: "Введите ваш телефон",
     name: "phone",
     required: true,
   });
@@ -91,14 +91,14 @@ const createModal = (title, description) => {
 
   const buttonSubmit = createElem("button", {
     className: "modal__button",
-    textContent: " Заказать",
+    textContent: "Заказать",
     type: "submit",
   });
 
   buttonSubmit.setAttribute("form", "order");
 
   const closeModalButton = createElem("button", {
-    className: " modal__close",
+    className: "modal__close",
     innerHTML: `<svg width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path d="M23.75 8.0125L21.9875 6.25L15 13.2375L8.0125 6.25L6.25 8.0125L13.2375 15L6.25 21.9875L8.0125 23.75L15 16.7625L21.9875 23.75L23.75 21.9875L16.7625 15L23.75 8.0125Z" fill="#00654E"/>
 </svg>
